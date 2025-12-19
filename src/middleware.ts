81a52beb1d4642844b39c5b7d4544b3f45c56e07
unknown <yonatanvigiliolavado09@gmail.com>
2025-12-18 22:55:59 -0500

    feat: change

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,15 @@
+import { PUBLIC_ENV } from "astro:env/client";
 import { defineMiddleware } from "astro:middleware";
-
 export const onRequest = defineMiddleware((context, next) => {
 	// Si estamos en desarrollo, los datos vienen por el Header 'x-astro-locals'
 	// que inyectó nuestro Proxy.
-
-	const headerData = context.request.headers.get("x-astro-locals");
-	if (headerData) {
-		// Rellenamos el contexto de Astro con los datos del Backend
-		const parsedData = JSON.parse(headerData);
-		for (const [key, value] of Object.entries(parsedData)) {
-			context.locals[key] = value;
-		}
-	}
+	// Rellenamos el contexto de Astro con los datos del Backend
+	const locals =
+		PUBLIC_ENV === "development"
+			? JSON.parse(context.request.headers.get("x-astro-locals") || "{}")
+			: context.locals;
+	Object.assign(context.locals, locals);
 
 	// En producción, context.locals ya viene lleno gracias al adaptador de Node.
-
 	return next();
 });
